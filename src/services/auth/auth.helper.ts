6d0a7de613fa998a90ae8bbfcc038a8c7132e5f8
Ruslan <ruslan.mirets@gmail.com>
2023-05-07 12:06:19 +0300

    api helper, types, api interceptor

---
+++ src/services/auth/auth.helper.ts
@@ -0,0 +1,26 @@
+import Cookies from "js-cookie";
+import { IAuthResponse, ITokens } from "@/store/user/user.interface";
+
+export const getAccessToken = () => {
+	const accessToken = Cookies.get("accessToken");
+	return accessToken || null;
+};
+
+export const getUserFromStorage = () => {
+	return JSON.parse(localStorage.getItem("user") || "{}");
+};
+
+export const saveTokenStorage = (data: ITokens) => {
+	Cookies.set("accessToken", data.accessToken);
+	Cookies.set("refreshToken", data.refreshToken);
+};
+
+export const removeTokensStorage = () => {
+	Cookies.remove("accessToken");
+	Cookies.remove("refreshToken");
+};
+
+export const saveToStorage = (data: IAuthResponse) => {
+	saveToStorage(data);
+	localStorage.setItem("user", JSON.stringify(data.user));
+};
